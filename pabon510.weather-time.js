be8e4a30bffd3b4eb9e512d81875eeb37865df5c
pabon510 <pabon510@gmail.com>
2025-08-21 20:06:13 -0400

    Add pabon510.weather-time.js

---
+++ pabon510.weather-time.js
@@ -0,0 +1,61 @@
+(function() {
+  // Ensure CSS is loaded once
+  var cssHref = 'https://raw.githubusercontent.com/pabon510/weather-time-main/main/dark-navy-card.css';
+  function ensureCss() {
+    if (!document.querySelector('link[href="' + cssHref + '"]')) {
+      var link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = cssHref;
+      document.head.appendChild(link);
+    }
+  }
+
+  class WeatherCard extends HTMLElement {
+    connectedCallback() {
+      ensureCss();
+      this.innerHTML = ''
+        + '<div class="wx-card">'
+        + '<div class="wx-header"><div class="wx-city">Washington, D.C.</div></div>'
+        + '<div class="wx-temp-row">'
+        + '<div><div class="wx-temp">23\u00b0C</div><div class="wx-condition">Few clouds</div></div>'
+        + '<img class="wx-icon" src="https://cdn-icons-png.flaticon.com/512/1163/1163624.png" alt="Few clouds">'
+        + '</div>'
+        + '<div class="wx-forecast">'
+        + '<div class="wx-row"><div class="wx-day">Friday</div><div class="wx-small"><img src="https://cdn-icons-png.flaticon.com/512/1163/1163634.png" alt="Few clouds" width="18" height="18">Few clouds</div><div class="wx-high">31\u00b0C</div></div>'
+        + '<div class="wx-row"><div class="wx-day">Saturday</div><div class="wx-small"><img src="https://cdn-icons-png.flaticon.com/512/1163/1163624.png" alt="Few clouds" width="18" height="18">Few clouds</div><div class="wx-high">30\u00b0C</div></div>'
+        + '<div class="wx-row"><div class="wx-day">Sunday</div><div class="wx-small"><img src="https://cdn-icons-png.flaticon.com/512/1163/1163623.png" alt="Few clouds" width="18" height="18">Few clouds</div><div class="wx-high">29\u00b0C</div></div>'
+        + '</div>'
+        + '</div>';
+    }
+  }
+
+  if (!customElements.get('weather-card')) {
+    customElements.define('weather-card', WeatherCard);
+  }
+
+  function factory(context) {
+    var card = document.createElement('weather-card');
+    return card;
+  }
+
+  var blockDefinition = {
+    name: 'weather-card',
+    factory: factory,
+    attributes: [],
+    label: 'Weather Card',
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/869/869869.png'
+  };
+
+  var externalBlockDefinition = {
+    blockDefinition: blockDefinition,
+    author: 'pabon510',
+    version: '1.0.0'
+  };
+
+  if (window.defineBlock) {
+    window.defineBlock(externalBlockDefinition);
+  } else {
+    var container = document.currentScript && document.currentScript.parentElement ? document.currentScript.parentElement : document.body;
+    container.appendChild(factory());
+  }
+})();
